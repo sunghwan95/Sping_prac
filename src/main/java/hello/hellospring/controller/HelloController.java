45bfece1df9be4d38d9908b1da0ec5430e68d37e
sunghwan95 <psung9510@gmail.com>
2024-03-08 07:17:49 +0900

    Feat: Class를 통한  API통신

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,25 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setValue(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getKKKKey(){
+            return name;
+        }
+
+        public void setValue(String name){
+            this.name=name;
+        }
     }
 }
